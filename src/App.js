7baadd396700772e1892a7bbd57f04dace851c50
nicomiguelino <nicomiguelino2014@gmail.com>
2021-05-04 21:57:33 +0800

    Add navbar.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+
+import "./App.scss";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+
       <div className="container">
         <div className="d-flex justify-content-center mt-3">
-          <h3>Hello, Twitter clone!</h3>
+          <h3>Hello there!</h3>
         </div>
 
         <div className="d-flex justify-content-center">
-          <h6 className="text-muted">The most-awaited clone is coming soon...</h6>
+          <h6 className="text-muted">
+            It's like Twitter, but green.
+          </h6>
         </div>
 
         <div className="d-flex justify-content-center mt-4">
-          <button className="btn btn-sm btn-info">
+          <button className="btn btn-success">
             Get Started
           </button>
         </div>
---
+++ src/components/Navigation.js
@@ -0,0 +1,35 @@
+import {
+  Navbar, Nav
+} from "react-bootstrap";
+
+function Navigation() {
+  return (
+    <Navbar
+      bg="success"
+      variant="dark"
+      expand="lg"
+      sticky="top"
+      className="mb-5 py-3 px-lg-5 px-4"
+    >
+      <Navbar.Brand
+        href="#home"
+        className="mr-4"
+      >
+        <strong>Twitter Clone</strong>
+      </Navbar.Brand>
+      <Navbar.Toggle
+        aria-controls="basic-navbar-nav"
+      />
+      <Navbar.Collapse
+        id="basic-navbar-nav"
+      >
+        <Nav className="ml-auto">
+          <Nav.Link href="#">Profile</Nav.Link>
+          <Nav.Link href="#">Notifications</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default Navigation;
