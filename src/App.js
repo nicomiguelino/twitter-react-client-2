0253e4a0d21c30f7d29f43ac7868e874b4d04ab3
nicomiguelino <nicomiguelino2014@gmail.com>
2021-05-07 12:43:19 +0800

    Change the title of the home page.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "./App.scss";
 import Navigation from "./components/Navigation";
 import Feed from "./components/Feed";
 
+const middot = '\u00b7';
+
 function App() {
+  useEffect(() => {
+    document.title = `Yet another Twitter clone ${middot}  Home`;
+  }, []);
+
   return (
     <div className="App">
       <Navigation />
