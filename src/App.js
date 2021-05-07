e6720268eebf76b81350a96ff92e8d2526ac4244
nicomiguelino <nicomiguelino2014@gmail.com>
2021-05-07 13:27:58 +0800

    Add support for URL routing.
    - Install react-router-dom.
    - Add routes for home and login.
    - Add a filler WIP page for login.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 
-import "./App.scss";
-import Navigation from "./components/Navigation";
-import Feed from "./components/Feed";
+import './App.scss';
+import Navigation from './components/Navigation';
+import Feed from './components/Feed';
 
 const middot = '\u00b7';
 
@@ -12,10 +18,24 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <Navigation />
-      <Feed />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home">
+            <Navigation />
+            <Feed />
+          </Route>
+          <Route path="/login">
+            <div className="container-fluid d-flex justify-content-center mt-5">
+              <h3>Login page is still a work in progress.</h3>
+            </div>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
